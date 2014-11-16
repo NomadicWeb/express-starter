db92b5d163f74e39b62421bc1dee9389acc83fc9
Luke Murphy <lukewm@riseup.net>
2014-11-16 00:37:15 -0200

    add CSS minifier + reset the local gulp version

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp   = require('gulp'),
     stylus = require('gulp-stylus'),
     axis   = require('axis'),
-    nmon   = require('gulp-nodemon');
+    nmon   = require('gulp-nodemon'),
+    minCSS = require('gulp-minify-css');
 
 gulp.task('stylus', function () {
   gulp.src('./app/assets/stylesheets/style.styl')
   .pipe(stylus({use: [axis()]}))
+  .pipe(minCSS())
   .pipe(gulp.dest('./app/assets/stylesheets'));
 });
 
