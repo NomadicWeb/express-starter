c59cead1cce184b7dc204b8aac591dea83da3979
Luke Murphy <lukewm@riseup.net>
2014-11-16 00:42:59 -0200

    add uglify to the pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,25 @@ var gulp   = require('gulp'),
     stylus = require('gulp-stylus'),
     axis   = require('axis'),
     nmon   = require('gulp-nodemon'),
-    minCSS = require('gulp-minify-css');
+    minCSS = require('gulp-minify-css'),
+    uglify = require('gulp-uglify');
 
 gulp.task('stylus', function () {
   gulp.src('./app/assets/stylesheets/style.styl')
   .pipe(stylus({use: [axis()]}))
   .pipe(minCSS())
-  .pipe(gulp.dest('./app/assets/stylesheets'));
+  .pipe(gulp.dest('./app/assets/stylesheets/dist'));
+});
+
+gulp.task('uglify', function () {
+  gulp.src('./app/assets/javascripts/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./app/assets/javascripts/dist'));
 });
 
 gulp.task('watch', function(){
     gulp.watch("./app/assets/stylesheets/*.styl", ['stylus']);
+    gulp.watch("./app/assets/javascripts/*.js",   ['uglify']);
 });
 
 gulp.task('dev', function() {
